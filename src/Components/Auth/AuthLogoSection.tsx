8149dff7c97cbc6968b3e55ea18cc625c0233970
JamilAmasa <jamilamasa@gmail.com>
2024-07-18 12:08:39 +0100

    feat(auth): make auth page responsive

--- src/Components/Auth/AuthLogoSection.tsx
+++ src/Components/Auth/AuthLogoSection.tsx
@@ -1,25 +1,17 @@
 import { useNavigate } from "react-router-dom";
-import logoIcon from "../../assets/icons/logoIcon.png";
+import Logo from "../Logo";
 
 type Props = {};
 
 const AuthLogoSection = (props: Props) => {
   const navigate = useNavigate();
   return (
-    <div className="bg-[#CCDEEB] flex-[1.2] p-5">
+    <div className="bg-[#CCDEEB] flex-[1.2] p-5 hidden md:block ">
       <button onClick={() => navigate(-1)} className="text-custom-blue">
         X Back
       </button>
       {/* Logo */}
-
-      <div className="flex gap-3 justify-center items-center h-full">
-        <img
-          src={logoIcon}
-          alt="Logo Icon"
-          className="w-[37.02px] h-[49.41px]"
-        />
-        <h1 className="text-custom-blue text-3xl">everlearn</h1>
-      </div>
+      <Logo />
     </div>
   );
 };
